595d211b320c0ca5c52bceeb39a6a109baf326df
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-14 22:42:28 +0400

    enums and Galaxy class added

---
+++ GalaxySolarSystem(Example of Indexers)/GalaxySolarSystem(Example of Indexers)/Enumerations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxySolarSystem_Example_of_Indexers_
+{
+    class Enumerations
+    {
+        public enum SolarSystem
+        {
+            Sun,
+
+
+        }
+
+        public enum Planets
+        {
+            Mars ,
+            Jupiter, 
+            Earth,
+            Uranus, 
+            Neptune, 
+            Saturn, 
+            Mercury, 
+            Venus, 
+            Sun, 
+            Moon, 
+            Pluto, 
+        }
+
+        public enum Galaxy
+        {
+            Andromeda,
+            MilkyWay
+        }
+    }
+}
---
+++ GalaxySolarSystem(Example of Indexers)/GalaxySolarSystem(Example of Indexers)/Galaxy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxySolarSystem_Example_of_Indexers_
+{
+    class Galaxy
+    {
+        private static int numberofplanets;
+        private string[] planets = new string[numberofplanets];
+
+        public int Numberofplanets
+        {
+            set
+            {
+               if(value > 0 && value < 15 ) numberofplanets = value;
+               else { throw new ArgumentOutOfRangeException(); }
+            }
+        }
+         
+        public string this[int index]
+        {
+            get    
+            {
+                return planets[index];
+            }
+            set   
+            {
+                planets[index] = value;
+            }
+        }
+    }
+}

a6a2ac7680f189ede3e0a4cd5900408523e482e5
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-14 22:07:25 +0400

    core

---
+++ GalaxySolarSystem(Example of Indexers)/GalaxySolarSystem(Example of Indexers)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxySolarSystem_Example_of_Indexers_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

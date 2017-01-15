8bc346cf799b83781124c822d4b35ec13941582a
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-15 16:37:59 +0400

    Fibonacci calculator

---
+++ FibonacciCalculator(Indexers example)/FibonacciCalculator(Indexers example)/Fibonacci.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciCalculator_Indexers_example_
+{
+    public class Fibonacci
+    {
+        public Fibonacci(int l)
+        {
+            if(l>16 || l<0) throw new ArgumentOutOfRangeException();
+            length = l;
+            Fibonac = new int[l];
+            Fibonac[0] = 0;
+            Fibonac[1] = 1;
+            for (int i = 2; i < l; i++)
+            {
+                Fibonac[i] = Fibonac[i - 1] + Fibonac[i - 2];
+            }
+        }
+
+        private int[] Fibonac;
+        private int length;
+
+        public int this[int index]
+        {
+            get
+            {              
+                if (index >=0 && index <= length) return Fibonac[index];
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+    }
+}
---
+++ FibonacciCalculator(Indexers example)/FibonacciCalculator(Indexers example)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciCalculator_Indexers_example_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // new Fibonacci series
+            Fibonacci NewFibonacci = new Fibonacci(10);
+            // get 5th member of series
+            Console.WriteLine(NewFibonacci[9]);
+            Console.ReadKey();
+        }
+    }
+}

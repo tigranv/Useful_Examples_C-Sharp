cbe8ef3a3a29d54be19bee86f1cd012f356dd042
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-16 22:50:17 +0400

    Usings removed

--- LatinToArmenianConverter/LatinToArmenianConverter/LatToArmTest.cs
+++ LatinToArmenianConverter/LatinToArmenianConverter/LatToArmTest.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LatinToArmenianConverter
 {
-    static class LatToArm
+    static class LatToArmTest
     {
         static void Main(string[] args)
         {

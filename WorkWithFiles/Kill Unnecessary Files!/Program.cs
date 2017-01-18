987dbc2531132df4ef6df23de5c480c9311d4bfd
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-18 23:02:23 +0400

    core

---
+++ WorkWithFiles/Kill Unnecessary Files!/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kill_Unnecessary_Files_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\Tigran PC\Desktop\TestDirectory");
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                switch (file.Name)
+                {
+                    case "Necessary1.txt":
+                        break;
+                    case "Necessary2.txt":
+                        break;
+                    case "Necessary3.txt":
+                        break;
+                    default:
+                        file.Delete();
+                        break;
+                }                    
+            }
+        }
+    }
+}

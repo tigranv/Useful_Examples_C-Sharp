23859b92086f433001b8a9f8d19fe17dbc666671
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-21 18:17:59 +0400

    changed

--- Directory Tree/Directory Tree/Program.cs
+++ Directory Tree/Directory Tree/Program.cs
@@ -6,31 +6,31 @@ namespace Directory_Tree
 {
     class Program
     {
-        public static void DirectoryTree(string path)
+        public static void DirectoryTree(string fullPath)
         {
-            string[] directories = path.Split('\\');
-            string path1 = "";
+            string[] directories = fullPath.Split('\\');
+            string subPath = "";
             int cursorUp = 0;
             int cursorLeft = 0;
 
             for (int i = 0; i < directories.Length - 1; i++)
             {
-                path1 += directories[i] + @"\";
-                string[] folders = Directory.GetDirectories(path1).Select(Path.GetFileName).ToArray();
-                string[] files = new DirectoryInfo(path1).GetFiles().Select(o => o.Name).ToArray();
+                subPath += directories[i] + @"\";
+                string[] folders = Directory.GetDirectories(subPath).Select(Path.GetFileName).ToArray();
+                string[] files = new DirectoryInfo(subPath).GetFiles().Select(o => o.Name).ToArray();
                 int j = 0;
-                int longestFolder = (folders).Where(s => s.Length == folders.Max(m => m.Length)).First().Length;
-                //int longestFle = (files).Where(s => s.Length == files.Max(m => m.Length)).First().Length;
-                //int longestName = (longestFle>=longestFolder?longestFle:longestFolder)<25? (longestFle >= longestFolder ? longestFle : longestFolder):20;
-                int longestName = longestFolder; //longestFolder <= 25 ? longestFolder : 26;
+                // int longestFolder = (folders).Where(s => s.Length == folders.Max(m => m.Length)).First().Length;
+                // int longestFle = (files).Where(s => s.Length == files.Max(m => m.Length)).First().Length;
+                int longestName = 25; //(longestFolder <= longestFle ? longestFle:longestFolder)<=25? (longestFolder <= longestFle ? longestFle : longestFolder) : 26;
 
-                foreach (string name in folders)
+                for (int k = 0; k < folders.Length; k++)
                 {
-                    
-                    if (name != directories[i + 1])
+                    folders[k] = folders[k].Length <= 25 ? folders[k] : (folders[k].Substring(0, 22) + "...");
+
+                    if (folders[k] != directories[i + 1])
                     {
-                        Console.SetCursorPosition(cursorLeft + i, cursorUp + j);
-                        Console.WriteLine("+" + name);
+                        Console.SetCursorPosition(cursorLeft, cursorUp + j);
+                        Console.WriteLine("+" + folders[k]);
                         j++;
                     }
                     else
@@ -39,27 +39,29 @@ namespace Directory_Tree
 
                         if (i != directories.Length - 2)
                         {
-                            Console.SetCursorPosition(cursorLeft + i, cursorUp + j);
-                            Console.WriteLine("-" + name + new string('-', longestName + 3 - directories[i + 1].Length) + "\u261B");
+                            Console.SetCursorPosition(cursorLeft, cursorUp + j);
+                            Console.WriteLine("-" + folders[k] + new string('-', longestName - directories[i + 1].Length) + "\u261B");
                             j++;
                         }
                         else
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.SetCursorPosition(cursorLeft + i, cursorUp + j);
-                            Console.WriteLine("***"+name+"***");
+                            Console.SetCursorPosition(cursorLeft, cursorUp + j);
+                            Console.WriteLine("***"+ folders[k] + "***");
                             Console.ForegroundColor = ConsoleColor.Gray;
                             j++;
                         }
                     }
                 }
 
-                //foreach (string name in files)
-                //{
-                //    if (name.Length < longestName) Console.WriteLine("+" + name);
-                //    else Console.WriteLine("Too long name");
-                //    j++;
-                //}
+                for(int k = 0; k <  files.Length; k++)
+                {
+                    files[k] = files[k].Length <= 25 ? files[k] : (files[k].Substring(0, 22) + "...");
+                    Console.SetCursorPosition(cursorLeft, cursorUp + j);
+                    Console.WriteLine("+" + files[k]);
+                    j++;
+                }
+
                 cursorUp += Array.IndexOf(folders, directories[i + 1]) + 1;
                 cursorLeft += longestName+3;
             }

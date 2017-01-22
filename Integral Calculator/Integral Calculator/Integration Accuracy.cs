a85ddf057784280de45083b26912a6ed3b295fa7
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-22 21:18:11 +0400

    enum

---
+++ Integral Calculator/Integral Calculator/Integration Accuracy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integral_Calculator
+{
+    enum Integration_Accuracy
+    {
+        Accuracy1,
+        Accuracy2,
+        Accuracy3,
+        Accuracy4,
+    }
+}
--- Integral Calculator/Integral Calculator/Program.cs
+++ Integral Calculator/Integral Calculator/Program.cs
@@ -11,23 +11,50 @@ namespace Integral_Calculator
 
     class Program
     {
-        static double Integrate(Func<double, double> f, double x1, double x2)
+        // Delegat that takes function with return type double and 1 double argument 
+        delegate double MyDelegate(double x);
+
+        // Finction that will be sended with delegat
+        static double MyFunction(double x)
         {
-            double h = (x2 - x1) / 10000;
+            return Math.Exp(x) + Math.Log10(x);
+        }
+
+
+        // Function for integral calculation , which takes delegate as argument
+        static double Integrate(MyDelegate f,  double x1, double x2, Integration_Accuracy a)
+        {
+            int accuracy = 0;
+            switch (a)
+            {
+                case Integration_Accuracy.Accuracy1:
+                    accuracy = 10;
+                    break;
+                case Integration_Accuracy.Accuracy2:
+                    accuracy = 100;
+                    break;
+                case Integration_Accuracy.Accuracy3:
+                    accuracy = 10000;
+                    break;
+                case Integration_Accuracy.Accuracy4:
+                    accuracy = 1000000;
+                    break;
+            }
+
+            double h = (x2 - x1) / accuracy;
             double res = (f(x1) + f(x2)) / 2;
-            for (int i = 1; i < 10000; i++)
+            for (int i = 1; i < accuracy; i++)
             {
                 res += f(x1 + i * h);
             }
             return h * res;
         }
 
-
-
         static void Main(string[] args)
         {
+            MyDelegate Fx = MyFunction;
 
-            double resut = Integrate(Math.Sin, 0, Math.PI);
+            double resut = Integrate(Math.Sin, 0, Math.PI, Integration_Accuracy.Accuracy2);
 
             Console.WriteLine(resut);
 

0f6b91a67c43bf8534ff2e215b2ca76b7a620dc0
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-22 22:04:44 +0400

    finished

---
+++ Integral Calculator/Integral Calculator/MathFunctions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integral_Calculator
+{
+    public class MathFunctions
+    {
+        // Function for integral calculation , which takes delegate as argument
+        public static double Integrate(MyDelegate f, double x1, double x2, Integration_Accuracy a)
+        {
+            int accuracy = 0;
+            switch (a)
+            {
+                case Integration_Accuracy.Accuracy1:
+                    accuracy = 10;
+                    break;
+                case Integration_Accuracy.Accuracy2:
+                    accuracy = 100;
+                    break;
+                case Integration_Accuracy.Accuracy3:
+                    accuracy = 10000;
+                    break;
+                case Integration_Accuracy.Accuracy4:
+                    accuracy = 1000000;
+                    break;
+            }
+
+            double h = (x2 - x1) / accuracy;
+            double res = (f(x1) + f(x2)) / 2;
+            for (int i = 1; i < accuracy; i++)
+            {
+                res += f(x1 + i * h);
+            }
+            return h * res;
+        }
+    }
+
+    public enum Integration_Accuracy
+    {
+        Accuracy1,
+        Accuracy2,
+        Accuracy3,
+        Accuracy4,
+    }
+}
--- Integral Calculator/Integral Calculator/Program.cs
+++ Integral Calculator/Integral Calculator/Program.cs
@@ -6,57 +6,36 @@ using System.Threading.Tasks;
 
 namespace Integral_Calculator
 {
-
-
+    // Delegat that takes function with return type double and 1 double argument 
+    public delegate double MyDelegate(double x);
+    // Instead of MyDelegate, we could use Func<double, double>
 
     class Program
     {
-        // Delegat that takes function with return type double and 1 double argument 
-        delegate double MyDelegate(double x);
-
         // Finction that will be sended with delegat
         static double MyFunction(double x)
         {
             return Math.Exp(x) + Math.Log10(x);
         }
 
-
-        // Function for integral calculation , which takes delegate as argument
-        static double Integrate(MyDelegate f,  double x1, double x2, Integration_Accuracy a)
-        {
-            int accuracy = 0;
-            switch (a)
-            {
-                case Integration_Accuracy.Accuracy1:
-                    accuracy = 10;
-                    break;
-                case Integration_Accuracy.Accuracy2:
-                    accuracy = 100;
-                    break;
-                case Integration_Accuracy.Accuracy3:
-                    accuracy = 10000;
-                    break;
-                case Integration_Accuracy.Accuracy4:
-                    accuracy = 1000000;
-                    break;
-            }
-
-            double h = (x2 - x1) / accuracy;
-            double res = (f(x1) + f(x2)) / 2;
-            for (int i = 1; i < accuracy; i++)
-            {
-                res += f(x1 + i * h);
-            }
-            return h * res;
-        }
-
         static void Main(string[] args)
         {
+            //giving pointer of MyFunction to MyDelegate 
             MyDelegate Fx = MyFunction;
 
-            double resut = Integrate(Math.Sin, 0, Math.PI, Integration_Accuracy.Accuracy2);
+            // Calling Integrate function from MathFunctions class and sending Myfunction with MyDelegate  to function Integrate 
+            double MyFxResult = MathFunctions.Integrate(Fx, 0, 5, Integration_Accuracy.Accuracy2);
 
-            Console.WriteLine(resut);
+            // inegration of Sin function from 0 to Pi mut give 2, we use different accuracies to see the result
+            double SinInteg1 = MathFunctions.Integrate(Math.Sin, 0, Math.PI, Integration_Accuracy.Accuracy1);
+            double SinInteg2 = MathFunctions.Integrate(Math.Sin, 0, Math.PI, Integration_Accuracy.Accuracy2);
+            double SinInteg3 = MathFunctions.Integrate(Math.Sin, 0, Math.PI, Integration_Accuracy.Accuracy3);
+            double SinInteg4 = MathFunctions.Integrate(Math.Sin, 0, Math.PI, Integration_Accuracy.Accuracy4);
+
+            Console.WriteLine($"Integral of Sin(x) from 0 to Pi in {1}- accuracy is {SinInteg1}");
+            Console.WriteLine($"Integral of Sin(x) from 0 to Pi in {2}- accuracy is {SinInteg2}");
+            Console.WriteLine($"Integral of Sin(x) from 0 to Pi in {3}- accuracy is {SinInteg3}");
+            Console.WriteLine($"Integral of Sin(x) from 0 to Pi in {4}- accuracy is {SinInteg4}");
 
             Console.ReadKey();
         }

a3d4ea0dc86b174e8ec8b786a175e6f6cecfb750
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-22 18:36:55 +0400

    delegats

---
+++ Integral Calculator/Integral Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integral_Calculator
+{
+
+
+
+    class Program
+    {
+        static double Integrate(Func<double, double> f, double x1, double x2)
+        {
+            double h = (x2 - x1) / 10000;
+            double res = (f(x1) + f(x2)) / 2;
+            for (int i = 1; i < 10000; i++)
+            {
+                res += f(x1 + i * h);
+            }
+            return h * res;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            double resut = Integrate(Math.Sin, 0, Math.PI);
+
+            Console.WriteLine(resut);
+
+            Console.ReadKey();
+        }
+    }
+}

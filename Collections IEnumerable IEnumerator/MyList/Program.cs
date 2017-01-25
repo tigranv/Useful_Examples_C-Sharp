632c429890e48d18730ef16dfa705aac5bc5b644
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-25 23:41:37 +0400

    MyList

---
+++ Collections IEnumerable IEnumerator/MyList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyList<int> list = new MyList<int>();
+            for (int i = 0; i < 5; i++)
+                list.Add(i);
+
+            Console.WriteLine("Arrey Length = {0}", list.Count);
+
+            foreach (int item in list)
+                Console.Write("{0}  ", item);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}

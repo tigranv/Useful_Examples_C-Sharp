b6ca1e1e8a43898baada463b9189bcdbfa893f32
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-25 23:53:30 +0400

    dictionary

---
+++ Collections IEnumerable IEnumerator/MyDictionary/MyDictionary.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue> : IEnumerable<object>, IEnumerator<object>
+    {
+        private readonly TKey[] key;
+        private readonly TValue[] value;
+        private readonly int lenght;
+
+        int position = -1;
+
+        # region Свойство длинны коллекции
+        public int Lenght
+        {
+            get { return lenght; }
+        }
+        #endregion
+
+        #region constructor
+        public MyDictionary(int n)
+        {
+            key = new TKey[n];
+            value = new TValue[n];
+            lenght = n;
+        }
+        #endregion
+
+        #region indexer
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < key.Length)
+                    return key[index] + " - " + value[index];
+                return "Index out of range";
+            }
+        }
+        #endregion
+
+        #region Methot for adding key-value
+        public void Add(int i, TKey k, TValue l)
+        {
+            key[i] = k;
+            value[i] = l;
+        }
+        #endregion
+
+        #region Realisation IEnumerator<T>
+        public bool MoveNext()
+        {
+            if (position < key.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public object Current
+        {
+            get { return key[position] + " " + value[position]; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return key[position] + " " + value[position]; }
+        }
+        #endregion
+
+        #region Realisation IEnumerable<T>
+
+        public IEnumerator<object> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+
+        #endregion
+    }
+}
---
+++ Collections IEnumerable IEnumerator/MyDictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dictionary = new MyDictionary<string, string>(5);
+
+            dictionary.Add(0, "սեղան", "table");
+            dictionary.Add(1, "խնձոր", "aplle");
+            dictionary.Add(2, "մատիտ", "pencil");
+            dictionary.Add(3, "արև", "sun");
+            dictionary.Add(4, "գիրք", "book");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("second note in dictionary:");
+            Console.WriteLine(dictionary[1]);
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("Length of dictionary:");
+            Console.WriteLine(dictionary.Lenght + " words");
+            Console.WriteLine(new string('-', 50));
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}

acfdebdbcd1a9d396c3c058d89b614c9f4a8cc5d
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-27 14:22:02 +0400

    Extention method

---
+++ Collections IEnumerable IEnumerator/GetArrayExtention/MyList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetArrayExtention
+{
+    class MyList<T> : IEnumerable<T>
+    {
+        T[] array = null;
+
+        public MyList()
+        {
+            array = new T[0];
+        }
+
+        public int Count
+        {
+            get { return array.Length; }
+        }
+
+        public void Add(T elem)
+        {
+            T[] arr = new T[array.Length + 1];
+            array.CopyTo(arr, 0);
+            arr[array.Length] = elem;
+            array = arr;
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+        }
+
+        int position = -1;
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < array.Length - 1)
+                {
+                    position++;
+                    yield return array[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Collections IEnumerable IEnumerator/GetArrayExtention/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetArrayExtention
+{
+    static class StaticClass
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            T[] array = new T[list.Count()];
+
+            int i = 0;
+
+            foreach (T item in list)
+            {
+                array[i] = item;
+                i++;
+            }
+
+            return array;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            MyList<int> list = new MyList<int>();
+
+            for (int i = 0; i < 8; i++)
+                list.Add(i);
+
+            int[] arr = list.GetArray();
+
+            foreach (int t in arr)
+                Console.Write("{0}  ", t);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}

1024a48e36af06497f2849a63577eac064fcd7c4
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-01-31 01:32:28 +0400

    Let Keyword added

---
+++ LINQ/Let Keyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Let_Keyword
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Let creates local variable, that can be used only in expretion
+            var query = from x in Enumerable.Range(0, 10)
+                        let innerRange = Enumerable.Range(0, 10)
+                        from y in innerRange
+                        select new { X = x, Y = y, Product = x * y };
+
+            foreach (var item in query)
+                Console.WriteLine("{0} * {1} = {2}", item.X, item.Y, item.Product);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}

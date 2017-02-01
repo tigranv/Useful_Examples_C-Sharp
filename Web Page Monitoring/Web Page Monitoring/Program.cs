87e18a6d2e173ce345a532ccefe339faf8ae55e3
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-01 18:59:55 +0400

    Spy

--- Web Page Monitoring/Web Page Monitoring/Program.cs
+++ Web Page Monitoring/Web Page Monitoring/Program.cs
@@ -8,36 +8,5 @@ using System.Threading.Tasks;
 
 namespace Web_Page_Monitoring
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string url = "http://www.micarmenia.am/";
 
-            WebClient client = new WebClient();
-            string html = client.DownloadString(url);
-
-            FindHrefs(html);
-
-            Console.ReadKey();
-        }
-
-
-        private static void FindHrefs(string input)
-        {
-            Regex regex = new Regex("href\\s*=\\s*(?:\"(?<1>[^\"]*)\"|(?<1>\\S+))", RegexOptions.IgnoreCase);
-            Match match;
-            for (match = regex.Match(input); match.Success; match = match.NextMatch())
-            {
-                
-                foreach (Group group in match.Groups)
-                {
-                    Console.WriteLine(group);
-                }
-            }
-
-        }
-
-
-    }
 }
---
+++ Web Page Spying/Web Page Spying/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Web_Page_Spying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://www.micarmenia.am/";
+
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+
+            FindHrefs(html);
+
+            Console.ReadKey();
+        }
+
+
+        private static void FindHrefs(string input)
+        {
+            Regex regex = new Regex("href\\s*=\\s*(?:\"(?<1>[^\"]*)\"|(?<1>\\S+))", RegexOptions.IgnoreCase);
+            Match match;
+            for (match = regex.Match(input); match.Success; match = match.NextMatch())
+            {
+
+                foreach (Group group in match.Groups)
+                {
+                    Console.WriteLine(group);
+                }
+            }
+
+        }
+
+
+    }
+}

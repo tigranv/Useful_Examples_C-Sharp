b56c7808612981f06ac297aceb8c8bee7fa896a8
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-01 17:22:24 +0400

    final

--- Garbage Collector/Nine Lives of a Cat/Program.cs
+++ Garbage Collector/Nine Lives of a Cat/Program.cs
@@ -10,28 +10,74 @@ namespace Nine_Lives_of_a_Cat
     {
         ~Cat()
         {
-            Console.WriteLine($"Finaliser {Program.counter++}");
+            Program.counter++;
+            if (Program.counter < 10) Console.WriteLine($"Killing cat!!!");
             Program.Instance = this;
 
             if (Program.counter < 9)
+            {
+                Console.WriteLine($"Myauuu , it was my {Program.counter} live");
                 GC.ReRegisterForFinalize(this);
+            }
+
+            if (Program.counter == 9)
+            {
+                Console.WriteLine($"MyauuuVaaaaax it was my {Program.counter} live, You Killed Me Completely ");
+                GC.ReRegisterForFinalize(this);
+            }
+
         }
     }
     class Program
     {
         public static Cat Instance { get; set; }
-        public static int counter;
+        public static int counter = 0;
 
         static void Main(string[] args)
         {
             // new Cat
             Instance = new Cat();
 
-            // Killing cut
-            Instance = null;
+            Instance = null; // Killing cut 1 time
+            GC.Collect();
+            Console.ReadKey();
 
+            Instance = null; // Killing cut 2 time
+            GC.Collect();
+            Console.ReadKey();
 
-            
+            Instance = null; // Killing cut 3 time
+            GC.Collect();
+            Console.ReadKey();
+
+            Instance = null; // Killing cut 4 time
+            GC.Collect();
+            Console.ReadKey();
+
+            Instance = null; // Killing cut 5 time
+            GC.Collect();
+
+            Instance = null; // Killing cut 6 time
+            GC.Collect();
+            Console.ReadKey();
+
+            Instance = null; // Killing cut 7 time
+            GC.Collect();
+            Console.ReadKey();
+
+            Instance = null; // Killing cut 8 time
+            GC.Collect();
+            Console.ReadKey();
+
+            Instance = null; // Killing cut 9 time
+            GC.Collect();
+            Console.ReadKey();
+
+            Instance = null; // Killing cut 1st time
+            GC.Collect();
+            Console.ReadKey();
+
+            Console.ReadKey();
         }
     }
 }

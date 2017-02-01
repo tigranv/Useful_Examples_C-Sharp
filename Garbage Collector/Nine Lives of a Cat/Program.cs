aad0116cdf9bf8ee83082ab1e2a6b973ebfc0c54
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-01 16:51:09 +0400

    lives of cat

---
+++ Garbage Collector/Nine Lives of a Cat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nine_Lives_of_a_Cat
+{
+    class Cat
+    {
+        ~Cat()
+        {
+            Console.WriteLine($"Finaliser {Program.counter++}");
+            Program.Instance = this;
+
+            if (Program.counter < 9)
+                GC.ReRegisterForFinalize(this);
+        }
+    }
+    class Program
+    {
+        public static Cat Instance { get; set; }
+        public static int counter;
+
+        static void Main(string[] args)
+        {
+            // new Cat
+            Instance = new Cat();
+
+            // Killing cut
+            Instance = null;
+
+
+            
+        }
+    }
+}

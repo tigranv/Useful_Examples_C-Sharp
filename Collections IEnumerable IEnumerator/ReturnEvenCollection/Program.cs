df3d1c41239fc9a96a4eff14d0573ad688543377
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-06 23:14:44 +0400

    generics

--- Collections IEnumerable IEnumerator/ReturnEvenCollection/Program.cs
+++ Collections IEnumerable IEnumerator/ReturnEvenCollection/Program.cs
@@ -29,8 +29,11 @@ namespace ReturnEvenCollection
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 41, 20, 87, 6, 9, 8, 3, 7,  };
             IEnumerable res = FindEven(array);
             foreach (int elem in res)
+            {
                 Console.Write("{0}  ", elem);
-            Console.Write("\n");
+            }
+
+
             Console.ReadKey();
         }
     }
---
+++ GenericProgramming/ClassFactory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFactory
+{
+    class MyClass<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int i = MyClass<int>.FacrotyMethod();
+
+            Console.WriteLine(i.GetType().Name);
+
+            Program p = MyClass<Program>.FacrotyMethod();
+
+            Console.WriteLine(p.GetType().Name);
+
+            Console.ReadKey();
+        }
+    }
+}

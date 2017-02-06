37a4a36ba54b12e613f0626f1f9b47b1d50c53bd
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-06 23:42:28 +0400

    Generic programming

---
+++ GenericProgramming/MyList/MyList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class MyList<T>
+    {
+        private T[] array;
+
+        public MyList()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T a)
+        {
+            T[] tempArray = new T[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            tempArray[array.Length] = a;
+            array = tempArray;
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+        }
+
+        public int Count
+        {
+            get { return array.Length; }
+        }
+
+        public void Clear()
+        {
+            array = new T[0];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((int)(object)array[i] == (int)(object)item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string line = null;
+            for (int i = 0; i < array.Length; i++)
+            {
+                line += array[i] + " ";
+            }
+            return "list length" + array.Length + " List elements" + line;
+        }
+    }
+}
---
+++ GenericProgramming/MyList/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    static class Program
+    {
+        static void Main()
+        {
+            // Methods Add, Count, Clear, Contains
+
+            Console.WriteLine("Enter list size");
+            string str = Console.ReadLine();
+            int u = string.IsNullOrEmpty(str) ? 0 : Convert.ToInt32(str);
+
+            var list = new MyList<int>();
+
+            var t = new Random();
+            for (int x = 0; x < u; x++)
+            {
+                list.Add(t.Next(100));
+            }
+
+            Console.WriteLine("Elements of list are");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 30));
+
+            //foreach (var item in list)  //is not working , no getenumerator
+            //{
+
+            //}
+
+            Console.WriteLine("Enter searching number");
+            if (list.Contains(Convert.ToInt32(Console.ReadLine())))
+            {
+                Console.WriteLine("List contains this number");
+            }
+            else
+            {
+                Console.WriteLine("no matchies");
+            }
+
+
+            var arr = list.GetArray();
+
+            foreach (var item in arr) // working because GetArray returns Array tape
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            var temp = new T[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                temp[i] = list[i];
+            }
+            return temp;
+        }
+    }
+}

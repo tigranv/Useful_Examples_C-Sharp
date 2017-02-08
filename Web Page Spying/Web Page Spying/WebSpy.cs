378c8328ce867a97392b52a0c2493f54634b0d00
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-09 01:26:11 +0400

    finalizer

--- Web Page Spying/Web Page Spying/WebSpy.cs
+++ Web Page Spying/Web Page Spying/WebSpy.cs
@@ -207,16 +207,19 @@ namespace Web_Page_Spying
                 // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
                 // TODO: set large fields to null.
                 // can be anmanaged resourcies need to close
+                client = null;
+                writer = null;
 
                 disposedValue = true;
             }
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~WebSpy() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
+        //TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+         ~WebSpy()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
 
         // This code added to correctly implement the disposable pattern.
         public void Dispose()

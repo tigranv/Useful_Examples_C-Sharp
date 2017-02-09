2cf361d33bc93647a590c10aa2cf11b14cb465ca
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-09 19:22:57 +0400

    Object methods overriding

---
+++ ICloneable Object MemberwiseClone/Object_Methods_Overriding/Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Methods_Overriding
+{
+    class Box
+    {
+        public Box(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null || this.GetType() != obj.GetType())
+            return false;
+
+            Box inst = obj as Box;
+            return  (inst.sideA == sideA) && (inst.sideB == sideB) && (inst.sideC == sideC);
+        }
+
+        public override string ToString()
+        {
+            return $"Box with sizes A = {sideA}, B = {sideB}, C = {sideC}"; ;
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)sideA ^ (int)sideB ^ (int)sideC;
+        }
+
+    }
+}
---
+++ ICloneable Object MemberwiseClone/Object_Methods_Overriding/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Methods_Overriding
+{
+    class Program
+    {
+        static void Main()
+        {
+            // creating 2 boxes
+            Box box1 = new Box(15, 12, 5);
+            Box box2 = new Box(7, 12, 5);
+
+            Console.WriteLine(box1.ToString()+"\n");
+            Console.WriteLine(box2.ToString());
+
+            Console.WriteLine(new string(' ', 30));
+
+            Console.WriteLine("box1 equals to box2 = {0} \n", box1.Equals(box2));
+
+            Console.WriteLine(box1.GetHashCode() == box2.GetHashCode() ? "hash Codes are equal" : "Hash Codes are different");
+
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}

b0b365e4530194cefbf1c40e78e679afcc43ca9d
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-02-09 10:59:09 +0400

    Fibo added

---
+++ GalaxySolarSystem(Example of Indexers)/Fibomacci Calculator Indexers Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fibomacci_Calculator_Indexers_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // new Fibonacci series
+            Fibonacci NewFibonacci = new Fibonacci(10);
+            // get 6th member of series
+            Console.WriteLine(NewFibonacci[6]);
+            Console.ReadKey();
+        }
+    }
+}

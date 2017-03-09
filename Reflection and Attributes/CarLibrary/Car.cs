b225d0a78acc58f53e8c0752fc3e8d001cd07d0d
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-09 14:48:37 +0400

    Core

---
+++ Reflection and Attributes/CarLibrary/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public abstract class Car : ICar
+    {
+        protected string name;
+        protected short currentSpeed;
+        protected short maxSpeed;
+        protected EngineState state;
+
+        protected Car()
+        {
+            state = EngineState.EngineAlive;
+        }
+
+        protected Car(string name, short maxSpeed, short currentSpeed)
+            : this()
+        {
+            this.name = name;
+            this.maxSpeed = maxSpeed;
+            this.currentSpeed = currentSpeed;
+        }
+
+        public abstract void Acceleration();
+        
+
+        public void Driver(string name, int age)
+        {
+            Console.WriteLine("Driver Name: {0}. Age: {1}", name, age);
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public short CurrentSpeed
+        {
+            get { return currentSpeed; }
+            set { currentSpeed = value; }
+        }
+
+        public short MaxSpeed
+        {
+            get { return maxSpeed; }
+        }
+
+        public EngineState EngineState
+        {
+            get { return state; }
+        }
+    }
+}
---
+++ Reflection and Attributes/CarLibrary/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public interface ICar
+    {
+        void Acceleration();
+        void Driver(string name, int age);
+    }
+}

91779b34e83dafc6c940408d457dad9217b2c460
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-09 16:21:50 +0400

    Finished

---
+++ Reflection and Attributes/ICar/ICar.cs
@@ -0,0 +1,10 @@
+﻿// it is just a supporting class library with one interface for example of LateBinding Inerface
+
+namespace CarLibrary
+{
+    public interface ICar
+    {
+        void Acceleration();
+        void Driver(string name, int age);
+    }
+}
---
+++ Reflection and Attributes/LateBinding Interface/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using CarLibrary;
+
+// Late Binding is tecnology that provides - get types run time, get their names and properties run time.
+// In early binding all this parts are done in compile time.
+
+namespace LateBinding_Interface
+{
+    class Program
+    {
+        static void Main()
+        {
+            Assembly assembly = null;
+
+            try
+            {
+                assembly = Assembly.Load("CarLibrary");
+
+                Type type = assembly.GetType("CarLibrary.MiniVan");
+
+                ICar carInstance = Activator.CreateInstance(type) as ICar;
+
+                if (carInstance != null)
+                {
+                    carInstance.Acceleration();
+
+                    carInstance.Driver("Shumaher", 26);
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+
+        // Late Binding Dynamic
+
+        //static void Main()
+        //{
+        //    Assembly assembly = null;
+
+        //    try
+        //    {
+        //        assembly = Assembly.Load("CarLibrary");
+        //        Type type = assembly.GetType("CarLibrary.MiniVan");
+
+                  // dynamic instance 
+        //        dynamic carInstance = Activator.CreateInstance(type);
+        //        carInstance.Acceleration();
+        //        carInstance.Driver("Shumaher", 26);
+        //    }
+        //    catch (FileNotFoundException e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //    }
+
+
+        //    Console.ReadKey();
+        //}
+    }
+}

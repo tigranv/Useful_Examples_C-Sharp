049dbf24429d6a15f0b3fb5516c571e6625d7aa7
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-10 00:13:04 +0400

    core

---
+++ Attributes/Create Attribute/MyAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Create_Attribute
+{
+    // it is nessesary to inherit from System.Attribute
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+
+    class MyAttribute : System.Attribute
+    {
+        private readonly string date;
+        public string Date
+        {
+            get { return date; }
+        }
+
+        public MyAttribute(string date)
+        {
+            this.date = date;
+        }
+
+        private int number;
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+    }
+}
---
+++ Attributes/Create Attribute/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+
+namespace Create_Attribute
+{
+    [My("1/31/2008", Number = 1)]
+    public class MyClass
+    {
+        [MyAttribute("2/31/2007", Number = 2)]
+        public static void Method()
+        {
+            Console.WriteLine("MyClass.Method()\n");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass my = new MyClass();
+            MyClass.Method();
+
+            // Analising the attributes if type.
+
+            Type type = typeof(MyClass);
+            object[] attributes = null;
+
+            MyAttribute attribute = null;
+
+
+            // Getting all attributes of type MyAttribute (false - dont show base class attributes).
+            attributes = type.GetCustomAttributes(false);
+
+            foreach (object attributeType in attributes)
+            {
+                attribute = attributeType as MyAttribute;
+                Console.WriteLine("Type Analising  : Number = {0}, Date = {1}", attribute.Number, attribute.Date);
+            }
+
+
+            // Attribute analising.
+
+            // getting methods public and static - Method.
+            MethodInfo method = type.GetMethod("Method", BindingFlags.Public | BindingFlags.Static);
+
+            // Getting all attributes of type MyAttribute (false - dont show base class attributes).
+            attributes = method.GetCustomAttributes(typeof(MyAttribute), false);
+
+            foreach (MyAttribute attrib in attributes)
+            {
+                Console.WriteLine("Type Analising: Number = {0}, Date = {1}", attrib.Number, attrib.Date);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}

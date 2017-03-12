2ebf25f40ead7cf98d396a7e4a3318c1c5cf33e2
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-13 00:50:20 +0400

    Polimorphic call

--- AcceleratorMagnets(InheritanceExample)/Testing/Program.cs
+++ AcceleratorMagnets(InheritanceExample)/Testing/Program.cs
@@ -7,20 +7,55 @@ namespace Testing
     {
         static void Main(string[] args)
         {
-            // creating 2 magnets
-            BendingMagnet Bending = new BendingMagnet(1, Material.SCMagnets, 10, BendingRadius.weak, FieldDirection.Left);
-            UndulatorMagnet Undulator = new UndulatorMagnet(1, Material.Ferromagnets, 15);
-            // get information about magnets
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("------------- Bending Magnet --------------");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Bending.GetInformation();
-            Bending.PlotTrajectory();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("------------- Undulator Magnet --------------");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Undulator.GetInformation();
-            Undulator.PlotTrajectory();
+
+            //// creating 2 magnets, virtual override polimorphism
+            //BendingMagnet Bending = new BendingMagnet(1, Material.SCMagnets, 10, BendingRadius.weak, FieldDirection.Left);
+            //UndulatorMagnet Undulator = new UndulatorMagnet(1, Material.Ferromagnets, 15);
+
+            //// get information about magnets
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine("------------- Bending Magnet --------------");
+            //Console.ForegroundColor = ConsoleColor.Gray;
+            //Bending.GetInformation();
+            //Bending.PlotTrajectory();
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine("------------- Undulator Magnet --------------");
+            //Console.ForegroundColor = ConsoleColor.Gray;
+            //Undulator.GetInformation();
+            //Undulator.PlotTrajectory();
+            //Console.ReadKey();
+
+
+            // correct way to use polimorphism
+            Magnet UserMagnet = null;
+            Console.WriteLine("Enter the magnet that You Need");
+            string magnetName = Console.ReadLine();
+            // Lets suppose that user can add other options too,(to note write more code)
+
+            switch (magnetName)
+            {
+                case "bending":
+                    UserMagnet = new BendingMagnet(1, Material.SCMagnets, 10, BendingRadius.weak, FieldDirection.Left);
+                    break;
+
+                case "undulator":
+                    UserMagnet = new UndulatorMagnet(1, Material.Ferromagnets, 15);
+                    break;
+
+                default:
+                    {
+                        Console.WriteLine("Error: Wrong magnet name");
+                        break;
+                    }
+            }
+
+            if(UserMagnet != null)
+            {
+                UserMagnet.PlotTrajectory();
+                UserMagnet.GetInformation();
+            }
+
+
             Console.ReadKey();
         }
     }
